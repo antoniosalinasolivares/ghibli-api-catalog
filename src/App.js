a7798b5944de7b4a7a67ecb9281bce5e57ca5950
antoniosalinasolivares <antonio.salinas1998@comunidad.unam.mx>
2021-01-23 02:15:13 -0600

    created abstraction for Ghibli API with AXIOS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Button, Card } from 'react-bootstrap'
+import React, {useState, useEffect }from 'react'
+import {getFilms} from './GhibliController'
 
 function App() {
+
+  const [loaded, setLoaded] = useState(false);
+  const [films, setFilms] = useState([])
+
+  useEffect(()=>{
+    getFilms(setFilms, setLoaded)
+  }, [])
+
   return (
     <>
-      
+      {loaded?
+      <p>{JSON.stringify(films)}</p>:
+      <p>"This is loading"</p>}
     </>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/GhibliController.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+const base_url = 'https://ghibliapi.herokuapp.com/'
+
+export const getFilms = async (setFilms, setLoaded) => {
+    try {
+        const films = await axios.get(base_url+'films')
+        setFilms(films.data)
+        setLoaded(true)
+    } catch (error) {
+        return false
+    }
+}

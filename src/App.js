74af6bdd4cefb4ab1fd8e89c9fe6341222114c04
antoniosalinasolivares <antonio.salinas1998@comunidad.unam.mx>
2021-01-24 19:29:42 -0600

    adding more data from the api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+// Bootstrap dependencies
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Navbar, Nav, Button } from 'react-bootstrap'
-import React, {useState, useEffect, useContext }from 'react'
-import { getFilms } from './GhibliController'
+import { Navbar, Nav, Form, option} from 'react-bootstrap'
+
+// router dependencies
+import {Link} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+
+// react dependencies
+import React, {useState, useEffect, useContext, useRef }from 'react'
+import {UserContext} from './UserContext'
+import useLocalStorage from './useLocalStorage'
+
+// API query abstraction
+import { getFilms, getPeople, getLocations, getSpecies, getVehicles } from './GhibliController'
+
+// subcomponents
 import { FilmList } from './FilmList'
 import { Splashscreen } from './Splashscreen'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import {UserContext} from './UserContext'
-import {Link} from 'react-router-dom'
-import useLocalStorage from './useLocalStorage'
 
 const App = () => {
 
@@ -23,14 +32,20 @@ const App = () => {
             <Nav.Link>
               <Link to={'/favs'}> Favs </Link>
             </Nav.Link>
+            <Nav.Link>
+              <Link to={'/custom'}> Search </Link>
+            </Nav.Link>
             <Nav.Link href="https://ghibliapi.herokuapp.com/">Ghibli API</Nav.Link>
             <Nav.Link href="https://www.omdbapi.com/">OMDb API</Nav.Link>
             <Nav.Link href="https://github.com/antoniosalinasolivares/ghibli-api-catalog">Source</Nav.Link>
           </Nav>
         </Navbar>
-        <Switch>
+        <Switch style={{
+                'padding':'10px 10px 10px 10px',
+        }}>
           <Route exact path='/' component={Home} />
           <Route path='/favs' component={Favs} />
+          <Route path='/custom' component={Custom} />
         </Switch>
       </UserContext.Provider>
     </Router>
@@ -51,7 +66,7 @@ const Home = () => {
       setTimeout(()=>{
         getFilms(setFilms, setLoaded)
         setBeenhere(true)
-      }, 2000)
+      }, 1500)
     }
   }, [])
 
@@ -84,6 +99,34 @@ const Favs = () => {
             </div>     
         </>
         )
+}
+
+const Custom = () => {
+
+  const [Collection, setCollection] = useState([])
+  const [query, setQuery] = useLocalStorage('query', 'people')
+
+
+
+  return(
+    <>
+      <div style={{
+                'padding':'10px 10px 10px 10px',
+            }}>
+        <Form>
+          <Form.Group controlId="exampleForm.ControlSelect1">
+            <Form.Label>Example select</Form.Label>
+              <Form.Control as="select">
+                <option>People</option>
+                <option>Locations</option>
+                <option>Species</option>
+                <option>Vehicles</option>
+              </Form.Control>
+            </Form.Group>
+          </Form>
+      </div>
+    </>
+  )
 }
 
 export default App
--- src/GhibliController.js
+++ src/GhibliController.js
@@ -12,3 +12,39 @@ export const getFilms = async (setFilms, setLoaded) => {
         return false
     }
 }
+
+export const getPeople = async (setCollection, setLoaded) => {
+    try {
+        const people = await axios.get(base_url+'people')
+        console.log(people.data)
+    } catch (error) {
+        return false
+    }
+}
+
+export const getLocation = async (setCollection, setLoaded) => {
+    try {
+        const locations = await axios.get(base_url+'locations')
+        console.log(locations.data)
+    } catch (error) {
+        return false
+    }
+}
+
+export const getSpecies = async (setCollection, setLoaded) => {
+    try {
+        const species = await axios.get(base_url+'species')
+        console.log(species.data)
+    } catch (error) {
+        return false
+    }
+}
+
+export const getVehicles = async (setCollection, setLoaded) => {
+    try {
+        const vehicles = await axios.get(base_url+'vehicles')
+        console.log(vehicles.data)
+    } catch (error) {
+        return false
+    }
+}
